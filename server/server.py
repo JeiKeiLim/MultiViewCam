e2d37101b8e47193295e60ffdeb739920bf18b68
Jongkuk Lim <lim.jeikei@gmail.com>
2018-10-22 18:14:28 +0900

    changed getting image from client to a function

--- server/server.py
+++ server/server.py
@@ -14,6 +14,22 @@ def recvall(sock, count):
         count -= len(newbuf)
     return buf
 
+def getFrame(conn, idx=1):
+	while(True):
+		#String형의 이미지를 수신받아서 이미지로 변환 하고 화면에 출력
+		length = recvall(conn, 16)
+		while(length is None):
+			length = recvall(conn, 16)
+
+		stringData = recvall(conn, int(length))
+		data = numpy.fromstring(stringData, dtype='uint8')
+		#s.close()
+		decimg=cv2.imdecode(data,1)
+		cv2.imshow("SERVER" + str(idx),decimg)
+		in_key = cv2.waitKey(1)
+		if(in_key == 'q'):
+			break
+
 #수신에 사용될 내 ip와 내 port번호
 TCP_IP = ''
 TCP_PORT = 5001
@@ -24,21 +40,7 @@ s.bind((TCP_IP, TCP_PORT))
 s.listen(True)
 
 conn, addr = s.accept()
-while(True):
-	#conn, addr = s.accept()
 
-	#String형의 이미지를 수신받아서 이미지로 변환 하고 화면에 출력
-	length = recvall(conn, 16)
-	while(length is None):
-		length = recvall(conn, 16)
-
-	#length = recvall(conn,16)
-	stringData = recvall(conn, int(length))
-	data = numpy.fromstring(stringData, dtype='uint8')
-	#s.close()
-	decimg=cv2.imdecode(data,1)
-	cv2.imshow('SERVER',decimg)
-	cv2.waitKey(1)
-#	break
+getFrame(conn, 1)
 
 s.close()
